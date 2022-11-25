31272ad02081cb6581f65c5e8e27f2e21a3872be
Chi Nguyen <chi.nguyen@live.com>
2022-11-25 03:27:44 +0200

    signup page design init

---
+++ frontend/ui/signup/signup.js
@@ -0,0 +1,6 @@
+"use strict";
+// change url when uploading to server
+const url = "http://localhost:3000";
+
+// select existing html elements
+const signupForm = document.querySelector("#signup-form");
