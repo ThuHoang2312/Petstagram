42c1ed5193fe033df6ef5ccdb2f5be85dfa30599
Chi Nguyen <chi.nguyen@live.com>
2022-12-02 21:58:58 +0200

    add logout.js

---
+++ frontend/ui/logout.js
@@ -0,0 +1,17 @@
+"use strict";
+import { url } from "config.js";
+
+export default async function logOut() {
+  try {
+    const response = await fetch(url + "/auth/logout");
+    const json = await response.json();
+
+    // remove token
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+    alert("You have logged out");
+    location.href = "../home/index.html";
+  } catch (e) {
+    console.log(e.message);
+  }
+}
