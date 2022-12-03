9dbd47b4d80a92e81efec4aca3107f93b19eda58
Chi Nguyen <chi.nguyen@live.com>
2022-12-03 15:44:17 +0200

    add login script

--- frontend/ui/login/login.js
+++ frontend/ui/login/login.js
@@ -1,6 +1,32 @@
 "use strict";
-//change when upload to server
-const url = "http://localhost:3000";
+
+import { url } from "../config.js";
 
 //select existing html element
 const loginForm = document.querySelector("#login-form");
+
+// login
+loginForm.addEventListener("submit", async (evt) => {
+  evt.preventDefault();
+  const data = serializeJson(loginForm);
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(url + "/auth/login", fetchOptions);
+  const json = await response.json();
+
+  if (json.user) {
+    // save token
+    sessionStorage.setItem("token", json.token);
+    sessionStorage.setItem("user", JSON.stringify(json.user));
+    location.href = "../front/index.html";
+    return;
+  }
+
+  alert(json.message);
+});
