8c8478f783448c154306e3e4a74f7738ff13c1e2
ThuHoang2312 <thu.hoang1223@gmail.com>
2022-12-07 19:31:53 +0200

    fix login error

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,19 @@
-"use strict"
-const express = require("express")
+'use strict'
+const express = require('express')
 const router = express.Router()
-const { login, register, logout } = require("../controllers/authController")
+const { login, register, logout } = require('../controllers/authController')
 const { body } = require('express-validator')
 
 // Route for registering and logging in
-router.get("/logout", logout)
-  .post("/login", login)
-  .post("/register", body('username').isLength({ min: 2 }).trim().escape(),
+router
+  .get('/logout', logout)
+  .post('/login', login)
+  .post(
+    '/register',
+    body('username').isLength({ min: 2 }).trim().escape(),
     body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min: 4 }).trim(), register)
+    body('password').isLength({ min: 4 }).trim(),
+    register
+  )
 
 module.exports = router
